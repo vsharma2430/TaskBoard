69ce2e9f17499196c0cfde458a62d5aabcc77fa3
Vaibhav Sharma <59431005+vsharma2430@users.noreply.github.com>
2025-02-25 15:31:28 +0530

    && added

--- server_app/misc.py
+++ server_app/misc.py
@@ -29,10 +29,20 @@ def add_s_tag(data:str):
         return '<s> ' + data
     else :
         return data
+    
+def join_strings_starting_with_and(strings:list[str]) -> list[str]:
+    modified_strings = []
+    for string in strings:
+        if string.startswith('&&'):
+            if modified_strings:
+                modified_strings[-1] += string[2:]
+        else:
+            modified_strings.append(string)
+    return modified_strings
 
 clean_list = lambda list_obj:list([x for x in list_obj if x is not None])
 clean_list_str = lambda list_obj:list([x for x in list_obj if (x is not None and x != '')])
-clean_list_str_diary = lambda list_obj:list([x for x in list_obj if (x is not None and x != '' and x.startswith('#') == False)])
+clean_list_str_diary = lambda list_obj:join_strings_starting_with_and(list([x for x in list_obj if (x is not None and x != '' and x.startswith('#') == False)]))
 list_map = lambda list_obj,fn=str.strip : list(map(fn,list_obj))
 date_display = lambda dtx : dtx.date().strftime("%d %b'%y")
 lag_display_days = lambda start,end: (end-start).days if (end > start) else -1*(start-end).days
